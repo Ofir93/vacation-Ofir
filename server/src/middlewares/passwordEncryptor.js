82900acf78070836cbea6da0ff82ff4b32300525
Ofir Ben Yacov <ofirby93@gmail.com.(none)>
2023-01-23 21:26:07 +0200

    for yuval

---
+++ server/src/middlewares/passwordEncryptor.js
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs'
+
+export default async (req, res, next) => {
+  try {
+    const salt = await genSalt()
+    console.log(salt)
+    const hashed = await hash(req.body.password, salt)
+    console.log(hashed)
+    res.locals.password = hashed
+    console.log(hashed)
+    next()
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+}
