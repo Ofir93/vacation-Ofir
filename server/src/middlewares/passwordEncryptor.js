baf41867c0782b0f11a1b46313c0ee06526f39e7
Ofir Ben Yacov <ofirby93@gmail.com.(none)>
2023-01-23 21:34:50 +0200

    yuval

--- server/src/middlewares/passwordEncryptor.js
+++ server/src/middlewares/passwordEncryptor.js
@@ -2,12 +2,24 @@ import bcrypt from 'bcryptjs'
 
 export default async (req, res, next) => {
   try {
-    const salt = await genSalt()
-    console.log(salt)
-    const hashed = await hash(req.body.password, salt)
-    console.log(hashed)
-    res.locals.password = hashed
-    console.log(hashed)
+    bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+        console.log(hash)
+
+    //   storeNewUser(firstName, lastName, userName, hash)
+    //   return res.status(200).send('registration comlplited')
+    })
+    .catch((err) => {
+      res.status(400).send({ error: err })
+    })
+
+    // const salt = await genSalt()
+    // console.log(salt)
+    // const hashed = await hash(req.body.password, salt)
+    // console.log(hashed)
+    // res.locals.password = hashed
+    // console.log(hashed)
     next()
   } catch (error) {
     console.error(error)
